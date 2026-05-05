447ac9901b02fc0f5573daed7937c3551c59dc6e
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-05-05 12:34:15 -0400

    config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+// ========== CONFIGURACIÓN DE LA TIENDA ==========
+// Cambia estos datos al venderle a otra empresa
+const TIENDA = {
+    nombre: "SiReBa",
+    rif: "291092270890",
+    moneda: "Bs.",
+    logo: "logo.png",
+    colorPrimario: "#8B5CF6",
+    colorSecundario: "#6D28D9",
+    firebase: {
+        apiKey: "AIzaSyDiqM2oykoa-EkTJoFPQSdejjqRthdWxQY",
+        authDomain: "saas-universal-app.firebaseapp.com",
+        projectId: "saas-universal-app",
+        storageBucket: "saas-universal-app.firebasestorage.app",
+        messagingSenderId: "1058808192755",
+        appId: "1:1058808192755:web:a1ebeb9a3625f42cd56186"
+    }
+};
