07f20b9d6f6dd73ec0cb80134e93507191f2c5fa
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-05-05 15:51:05 -0400

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// ========== CONFIGURACIÓN DE LA TIENDA ==========
-// Cambia estos datos al venderle a otra empresa
 const TIENDA = {
     nombre: "SiReBa",
     rif: "291092270890",
@@ -7,7 +5,6 @@ const TIENDA = {
     logo: "logo.png",
     colorPrimario: "#8B5CF6",
     colorSecundario: "#6D28D9",
-    webhookUrl: "https://TU-TUNEL.trycloudflare.com/webhook/siemba-webhook",
     firebase: {
         apiKey: "AIzaSyDiqM2oykoa-EkTJoFPQSdejjqRthdWxQY",
         authDomain: "saas-universal-app.firebaseapp.com",
@@ -15,5 +12,6 @@ const TIENDA = {
         storageBucket: "saas-universal-app.firebasestorage.app",
         messagingSenderId: "1058808192755",
         appId: "1:1058808192755:web:a1ebeb9a3625f42cd56186"
-    }
+    },
+    webhookUrl: "https://TU-N8N.com/webhook/siembra-webhook"
 };
